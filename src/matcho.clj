fafb5f0670e0167cc9dbaece60e1d8bb72e06131
Vladislav Ganshin <ganshinv@gmail.com>
2019-09-08 23:41:15 +0300

    Support clojure.core functions in dsl

--- src/matcho.clj
+++ src/matcho.clj
@@ -1,5 +1,6 @@
 
-(ns matcho)
+(ns matcho
+  (:require [clojure.string :as s]))
 
 (defn smart-explain-data [p x]
   (cond
@@ -70,10 +71,51 @@
               (conj errors (assoc err :path path))
               errors))))
 
+(defn built-in-fn [fn-name]
+  (if-let [func (ns-resolve 'clojure.core (symbol fn-name))]
+    #( func %)
+    (throw  (ex-info (str "Unknown function name '" fn-name "'") {:type :unknown-fn-name}))))
+
+(defn replace-with-functions [template]
+  (cond
+    (and (string? template) (s/ends-with? template "?") (built-in-fn template)) (built-in-fn template)
+    (and (string? template) (s/starts-with? template "#")) #(re-matches (java.util.regex.Pattern/compile (subs template 1)) %)
+    (map? template) (reduce-kv #(assoc %1 %2 (replace-with-functions %3)) {} template)
+    ;; list?
+    :else template))
+
+(comment
+  (replace-with-functions {:fn number? :str "number?"})
+
+  (replace-with-functions {:headerrs [] :status 200 :body {:name "str?"}})
+
+  (reduce-kv #(assoc %1 %2 (+ 1 %3)) {} {:a 1 :b 2})
+
+  (cond (and (str? template) (not (nil? (get funcs template)))) (get funcs template))
+
+  (println "hello!!!")
+
+  (#(re-matches (java.util.regex.Pattern/compile "aaa") %) "aaa")
+
+  (def wow #(re-matches (java.util.regex.Pattern/compile "aaa") %))
+
+  (wow "aaa")
+
+  (java.util.regex.Pattern/compile "aaa")
+
+  ((resolve "string?") "wow")
+
+
+  (match {:a true} {:a number?})
+
+  )
+
+
+
 (defn match
   "Match against each pattern"
   [x & patterns]
-  (reduce (fn [acc pattern] (match-recur acc [] x pattern)) [] patterns))
+  (reduce (fn [acc pattern] (match-recur acc [] x (replace-with-functions pattern))) [] patterns))
 
 
 
