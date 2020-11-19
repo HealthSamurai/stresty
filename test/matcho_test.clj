586356b37102f143b4e98736b76c19ee80d950a5
Stas Buldakov <xamelon96@gmail.com>
2020-11-19 18:27:08 +0400

    add test for matcho templates

--- test/matcho_test.clj
+++ test/matcho_test.clj
@@ -14,28 +14,32 @@
 ;;        (is true))))
 
 (deftest test-matcho
-  (is (empty? (sut/match {:a 1} {:a 1})))
+  (is (empty? (sut/match {} {:a 1} {:a 1})))
 
   (is (=
-       (sut/match {:a 1} {:a 2})
+       (sut/match {} {:a 1} {:a 2})
        [{:expected 2 :but 1 :path [:a]}]))
 
-  (is (empty? (sut/match {:a 1} {:a number?})))
+  (is (empty? (sut/match {} {:a 1} {:a number?})))
 
-  (is (empty? (sut/match {:a 1} {:a "number?"})))
+  (is (empty? (sut/match {} {:a 1} {:a "number?"})))
 
-  (is (empty? (sut/match {:a "str"} {:a "#str"})))
+  (is (empty? (sut/match {} {:a "str"} {:a "#str"})))
 
-  (is (empty? (sut/match {:a 200} {:a "2xx?"})))
+  (is (empty? (sut/match {} {:a 200} {:a "2xx?"})))
 
-  (is (empty? (sut/match {:a 204} {:a "2xx?"})))
+  (is (empty? (sut/match {} {:a 204} {:a "2xx?"})))
 
-  (is (empty? (sut/match {:a "string"} {:a "#str\\w+"})))
+  (is (empty? (sut/match {} {:a "string"} {:a "#str\\w+"})))
 
-  (is (= (sut/match {:a 404} {:a "2xx?"}) [{:expected "2xx?" :but 404 :path [:a]}]))
+  (is (= (sut/match {} {:a 404} {:a "2xx?"}) [{:expected "2xx?" :but 404 :path [:a]}]))
 
-  (is (empty? (sut/match {:a {:b 1}} {:a {:c "nil?"}})))
+  (is (empty? (sut/match {} {:a {:b 1}} {:a {:c "nil?"}})))
 
-  (is (empty? (sut/match [{:n "a"} {:n "b"} {:n "c"}] [{:n "a"} "any?" {:n "c"}])))
+  (is (empty? (sut/match {} [{:n "a"} {:n "b"} {:n "c"}] [{:n "a"} "any?" {:n "c"}])))
+
+  (is (empty? (sut/match {:user {:data {:patient_id "new-patient"}}}
+                         {:body {:id "new-patient"}}
+                         {:body {:id "{user.data.patient_id}"}})))
 
   )
