efaa9b6b982bda877b24184aabf0d5d9782f1ce1
Denis Shkabatur <denis6705@gmail.com>
2020-12-10 18:40:49 +0300

    add preparing case in for tutorial

--- src/core.clj
+++ src/core.clj
@@ -70,17 +70,21 @@
           ctx        (-> (opts :options)
                          (assoc :config config)
                          (assoc :test-cases test-cases))]
-      (prn "opts: " opts)
       (if (-> opts :options :ui)
         (do
           (swap! *ctx assoc :ztx ztx)
           (prn "starting server... ")
           (server/start *ctx)
           (println "UI started on localhost:8080"))
-        (clojure.pprint/pprint (runner/run ctx))))))
+        ;; print only responses with errors
+        (->> (runner/run ctx)
+            first
+            (filterv #(seq (:errors %)))
+            clojure.pprint/pprint)
+        ))))
 
 (comment
-  (-main "--ui" "--config" "resources/user.edn" "--file" "resources/user.edn")
+  (-main "--config" "resources/user.edn" "--file" "resources/user.edn")
 
   (parse-opts ["--config" "resources/user.edn" "--file" "wow/file.txt,some-file.end"] cli-options))
 
