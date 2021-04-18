46bee52c4c492c72e36a2d8a44b7243d901cece3
niquola <niquola@gmail.com>
2021-04-18 07:51:03 +0300

    start refactor

--- src/stresty/core.clj
+++ src/stresty/core.clj
@@ -1,11 +1,2 @@
-(ns stresty.core
-  (:require [zen.core :as zen]
-            [stresty.web.core :as server]))
+(ns stresty.core)
 
-(defonce *context (atom {:ztx (zen.core/new-context)}))
-
-(defn -main [& args]
-  (prn "starting server... ")
-  (server/start *context)
-
-  )
